823a779c6b5d33d19201af4926bd29b483aeb591
umutbasal <umut.bsl@yandex.com>
2022-07-12 02:28:54 +0300

    code

--- ocr.go
+++ ocr.go
@@ -4,14 +4,11 @@ import (
 	"github.com/otiai10/gosseract/v2"
 )
 
-func ocr(path string) string {
+func ocr(path string) (string, error) {
 	client := gosseract.NewClient()
 	defer client.Close()
+
 	client.SetImage(path)
 	client.SetLanguage("eng")
-	text, err := client.Text()
-	if err != nil {
-		panic(err)
-	}
-	return text
+	return client.Text()
 }
